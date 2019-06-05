c379c9af6135b0270d71c7ef878e032958ad4555
Carl Björknäs <carl.bjorknas@knowit.se>
2019-06-05 22:36:00 +0200

    Initial commit. Project created with a first test that cannot be run because "Program has more than one entry point defined.".

---
+++ TheRayTracerChallenge/Tests/TupleTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheRayTracerChallenge.Tests
+{
+    [TestFixture]
+    public class TupleTests
+    {
+        [Test]
+        public void A_tuple_with_w_equal_to_1_is_a_point()
+        {
+            var t = new Tuple(4.3, -4.2, 3.1, 1.0);
+            Assert.IsTrue(t.IsPoint);
+        }
+    }
+}
---
+++ TheRayTracerChallenge/Tuple.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheRayTracerChallenge
+{
+    struct Tuple
+    {
+        double x, y, z, w;
+
+        public Tuple(double x, double y, double z, double w)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
+        }
+
+        public bool IsPoint => w == 1.0;
+    }
+}
