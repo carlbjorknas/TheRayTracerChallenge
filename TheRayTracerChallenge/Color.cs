c16db8ff03da36cb23b0ecf0814be984a49a7019
Carl Björknäs <carl.bjorknas@knowit.se>
2019-06-06 19:56:13 +0200

    Adds color class and tests.

---
+++ TheRayTracerChallenge/Color.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheRayTracerChallenge
+{
+    struct Color
+    {
+        double r, g, b;
+
+        public Color(double r, double g, double b)
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+
+        public static Color operator +(Color c1, Color c2)
+        {
+            return new Color(c1.r + c2.r, c1.g + c2.g, c1.b + c2.b);
+        }
+
+        public static Color operator -(Color c1, Color c2)
+        {
+            return new Color(c1.r - c2.r, c1.g - c2.g, c1.b - c2.b);
+        }
+
+        public static Color operator *(Color c, double d)
+        {
+            return new Color(d * c.r, d * c.g, d * c.b);
+        }
+
+        public static Color operator *(Color c1, Color c2)
+        {
+            return new Color(c1.r * c2.r, c1.g * c2.g, c1.b * c2.b);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return
+                obj is Color t &&
+                Equals(t.r, r) &&
+                Equals(t.g, g) &&
+                Equals(t.b, b);
+        }
+
+        private bool Equals(double d1, double d2)
+        {
+            return Math.Abs(d1 - d2) < 0.00001;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(r, g, b);
+        }
+    }
+}
---
+++ TheRayTracerChallenge/Tests/ColorTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheRayTracerChallenge.Tests
+{
+    [TestFixture]
+    public class ColorTests
+    {
+        [Test]
+        public void Adding_colors()
+        {
+            var c1 = new Color(0.9, 0.6, 0.75);
+            var c2 = new Color(0.7, 0.1, 0.25);
+            var sum = c1 + c2;
+            Assert.AreEqual(new Color(1.6, 0.7, 1.0), sum);
+        }
+
+        [Test]
+        public void Subtracting_colors()
+        {
+            var c1 = new Color(0.9, 0.6, 0.75);
+            var c2 = new Color(0.7, 0.1, 0.25);
+            var diff = c1 - c2;
+            Assert.AreEqual(new Color(0.2, 0.5, 0.5), diff);
+        }
+
+        [Test]
+        public void Multiplying_a_color_with_a_scalar()
+        {
+            var c = new Color(0.2, 0.3, 0.4);
+            var scaled = c * 2;
+            Assert.AreEqual(new Color(0.4, 0.6, 0.8), scaled);
+        }
+
+        [Test]
+        public void Multiplying_colors()
+        {
+            var c1 = new Color(1, 0.2, 0.4);
+            var c2 = new Color(0.9, 1, 0.1);
+            var product = c1 * c2;
+            Assert.AreEqual(new Color(0.9, 0.2, 0.04), product);
+        }
+    }
+}
