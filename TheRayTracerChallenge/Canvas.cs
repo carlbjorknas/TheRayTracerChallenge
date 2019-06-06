df85760545e34ad018c346eadc618e0fab8fe1d5
Carl Björknäs <carl.bjorknas@knowit.se>
2019-06-06 20:32:29 +0200

    Adds a canvas class and tests.

---
+++ TheRayTracerChallenge/Canvas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheRayTracerChallenge
+{
+    class Canvas
+    {
+        readonly Color[,] _colors;
+
+        public Canvas(int width, int height)
+        {
+            _colors = new Color[width, height];
+        }
+
+        public int Width => _colors.GetLength(0);
+
+        public int Height => _colors.GetLength(1);
+
+        public Color PixelAt(int x, int y) 
+            => _colors[x, y];
+
+        internal void WritePixel(int x, int y, Color c) 
+            => _colors[x, y] = c;
+    }
+}
---
+++ TheRayTracerChallenge/Tests/CanvasTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheRayTracerChallenge.Tests
+{
+    [TestFixture]
+    public class CanvasTests
+    {
+        [Test]
+        public void Creating_a_canvas()
+        {
+            var canvas = new Canvas(10, 20);
+            Assert.AreEqual(10, canvas.Width);
+            Assert.AreEqual(20, canvas.Height);
+
+            var black = new Color(0, 0, 0);
+            var cols = Enumerable.Range(0, 10);
+            var rows = Enumerable.Range(0, 20);
+            cols
+                .SelectMany(x => rows, (x, y) => new { x, y })
+                .Select(pair => canvas.PixelAt(pair.x, pair.y))
+                .ToList()
+                .ForEach(pixelColor => Assert.AreEqual(black, pixelColor));
+        }
+
+        [Test]
+        public void Writing_pixels_to_a_canvas()
+        {
+            var canvas = new Canvas(10, 20);
+            var red = new Color(1, 0, 0);
+            canvas.WritePixel(2, 3, red);
+            Assert.AreEqual(red, canvas.PixelAt(2, 3));
+        }
+    }
+}
