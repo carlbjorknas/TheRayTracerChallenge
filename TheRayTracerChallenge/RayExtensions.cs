d5d85ab044f93ae849a78a32c2f0a5bc71a10321
Carl Björknäs <carl.bjorknas@knowit.se>
2019-07-06 15:50:50 +0200

    Moves intersection code to Sphere instead of Ray as it is in the book.

--- TheRayTracerChallenge/RayExtensions.cs
+++ TheRayTracerChallenge/RayExtensions.cs
@@ -11,25 +11,5 @@ namespace TheRayTracerChallenge
         {
             return ray.ThroughPoint + ray.Direction.ScaleBy(t);
         }
-
-        public static double[] Intersect(this Ray3D ray, Sphere sphere)
-        {
-            var sphereToRay = ray.ThroughPoint - sphere.Center;
-            var a = ray.Direction.DotProduct(ray.Direction);
-            var b = 2 * (ray.Direction.DotProduct(sphereToRay));
-            var c = sphereToRay.DotProduct(sphereToRay) - 1;
-            var discriminant = b * b - 4 * a * c;
-
-            var intersects = discriminant >= 0;
-            if (!intersects)
-            {
-                return new double[] { };
-            }
-
-            var t1 = (-b - Math.Sqrt(discriminant)) / (2 * a);
-            var t2 = (-b + Math.Sqrt(discriminant)) / (2 * a);
-
-            return new[] { t1, t2 };
-        }
     }
 }
--- TheRayTracerChallenge/Sphere.cs
+++ TheRayTracerChallenge/Sphere.cs
@@ -10,13 +10,33 @@ namespace TheRayTracerChallenge
         public static Sphere UnitSphere()
             => new Sphere(new Point3D(0, 0, 0));
 
+        Guid Id;
+        public Point3D Center;
+
         public Sphere(Point3D center)
         {
             Id = Guid.NewGuid();
             Center = center;
         }
 
-        Guid Id;
-        public Point3D Center;       
+        public double[] Intersect(Ray3D ray)
+        {
+            var sphereToRay = ray.ThroughPoint - Center;
+            var a = ray.Direction.DotProduct(ray.Direction);
+            var b = 2 * (ray.Direction.DotProduct(sphereToRay));
+            var c = sphereToRay.DotProduct(sphereToRay) - 1;
+            var discriminant = b * b - 4 * a * c;
+
+            var intersects = discriminant >= 0;
+            if (!intersects)
+            {
+                return new double[] { };
+            }
+
+            var t1 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+            var t2 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+
+            return new[] { t1, t2 };
+        }
     }
 }
