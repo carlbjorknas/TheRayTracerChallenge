b130f2597d434e4bb3f69fe140a1b96019e47e6b
Carl Björknäs <carl.bjorknas@knowit.se>
2019-07-07 18:04:18 +0200

    Adds printing of a sphere silhouette.

--- TheRayTracerChallenge/Program.cs
+++ TheRayTracerChallenge/Program.cs
@@ -11,6 +11,7 @@ namespace TheRayTracerChallenge
 
             PrintAPixelToACanvas();
             PrintClockFace();
+            PrintSphereSilhouette();
         }
 
         private static void PrintAPixelToACanvas()
@@ -38,6 +39,47 @@ namespace TheRayTracerChallenge
 
             var ppm = canvas.ToPpm();
             File.WriteAllText("clock_face.ppm", ppm);
+        }
+
+        private static void PrintSphereSilhouette()
+        {                        
+            var ray_origin = Tuple.Point(0, 0, -5);
+            var wall_z = 10;
+            const double wall_size = 7.0;
+            const int canvas_pixels = 100;
+            var pixel_size = wall_size / canvas_pixels;
+            var half = wall_size / 2;
+
+            var canvas = new Canvas(canvas_pixels, canvas_pixels);
+            var color = new Color(1, 0, 0);
+            var shape = Sphere.UnitSphere();
+
+            for (var y=0; y<canvas_pixels; y++)
+            {
+                // compute the world y coordinate (top = +half, bottom = -half)
+                var world_y = half - pixel_size * y;
+
+                for (var x=0; x<canvas_pixels; x++)
+                {
+                    // compute the world x coordinate (left = -half, right = half)
+                    var world_x = -half + pixel_size * x;
+
+                    // describe the point on the wall that the ray will target
+                    var position = Tuple.Point(world_x, world_y, wall_z);
+
+                    var ray = new Ray(ray_origin, (position - ray_origin).Normalize);
+                    var intersections = shape.Intersect(ray);
+
+                    if (intersections.Hit() != null)
+                    {
+                        canvas.WritePixel(x, y, color);
+                    }                    
+                }
+            }
+
+
+            canvas.WritePixel(0, 0, color);
+            File.WriteAllText("sphere_silhoutte.ppm", canvas.ToPpm());
         }
     }
 }
