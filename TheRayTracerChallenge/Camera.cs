ab216838363e77cff6d3e2fb2ae50f02db6e7016
Carl Björknäs <carl.bjorknas@knowit.se>
2019-09-08 08:52:10 +0200

    Adds a camera class (without functionality).

---
+++ TheRayTracerChallenge/Camera.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheRayTracerChallenge
+{
+    class Camera
+    {
+        public Camera(int hSize, int vSize, double fieldOfView)
+        {
+            HSize = hSize;
+            VSize = vSize;
+            FieldOfView = fieldOfView;
+            Transform = Transformation.Identity;
+        }
+
+        public double HSize { get; }
+        public double VSize { get; }
+        public double FieldOfView { get; }
+        public Transformation Transform { get; }
+    }
+}
---
+++ TheRayTracerChallenge/Tests/CameraTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheRayTracerChallenge.Tests
+{
+    [TestFixture]
+    public class CameraTests
+    {
+        [Test]
+        public void Contruction_a_camera()
+        {
+            var hSize = 160;
+            var vSize = 120;
+            var fieldOfView = Math.PI / 2;
+
+            var camera = new Camera(hSize, vSize, fieldOfView);
+
+            Assert.AreEqual(hSize, camera.HSize);
+            Assert.AreEqual(vSize, camera.VSize);
+            Assert.AreEqual(fieldOfView, camera.FieldOfView);
+            Assert.AreEqual(Transformation.Identity, camera.Transform);
+        }
+    }
+}
