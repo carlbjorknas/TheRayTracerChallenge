bfab2f7bbba5788ab7642b4b4e1050e3149811e1
Carl Björknäs <carl.bjorknas@knowit.se>
2019-10-13 20:34:52 +0200

    Adds a striped pattern class.

---
+++ TheRayTracerChallenge/StripePattern.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheRayTracerChallenge
+{
+    class StripePattern
+    {
+        public StripePattern(Color color1, Color color2)
+        {
+            Color1 = color1;
+            Color2 = color2;
+        }
+
+        public Color Color1 { get; }
+        public Color Color2 { get; }
+
+        internal Color StripeAt(Tuple point)
+            => Math.Floor(point.x) % 2 == 0 
+                ? Color1 
+                : Color2;
+    }
+}
---
+++ TheRayTracerChallenge/Tests/PatternTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheRayTracerChallenge.Tests
+{
+    [TestFixture]
+    class PatternTests
+    {
+        [Test]
+        public void Creating_a_stripe_pattern()
+        {
+            var pattern = new StripePattern(Color.White, Color.Black);
+            Assert.AreEqual(Color.White, pattern.Color1);
+            Assert.AreEqual(Color.Black, pattern.Color2);
+        }
+
+        [Test]
+        public void A_stripe_pattern_is_constant_in_y()
+        {
+            var pattern = new StripePattern(Color.White, Color.Black);
+            Assert.AreEqual(Color.White, pattern.StripeAt(Tuple.Point(0, 0, 0)));
+            Assert.AreEqual(Color.White, pattern.StripeAt(Tuple.Point(0, 1, 0)));
+            Assert.AreEqual(Color.White, pattern.StripeAt(Tuple.Point(0, 2, 0)));
+        }
+
+        [Test]
+        public void A_stripe_pattern_is_constant_in_z()
+        {
+            var pattern = new StripePattern(Color.White, Color.Black);
+            Assert.AreEqual(Color.White, pattern.StripeAt(Tuple.Point(0, 0, 0)));
+            Assert.AreEqual(Color.White, pattern.StripeAt(Tuple.Point(0, 0, 1)));
+            Assert.AreEqual(Color.White, pattern.StripeAt(Tuple.Point(0, 0, 2)));
+        }
+
+        [Test]
+        public void A_stripe_pattern_alternates_in_x()
+        {
+            var pattern = new StripePattern(Color.White, Color.Black);
+            Assert.AreEqual(Color.White, pattern.StripeAt(Tuple.Point(0, 0, 0)));
+            Assert.AreEqual(Color.White, pattern.StripeAt(Tuple.Point(0.9, 0, 0)));
+            Assert.AreEqual(Color.Black, pattern.StripeAt(Tuple.Point(1, 0, 0)));
+            Assert.AreEqual(Color.Black, pattern.StripeAt(Tuple.Point(-0.1, 0, 0)));
+            Assert.AreEqual(Color.Black, pattern.StripeAt(Tuple.Point(-1, 0, 0)));
+            Assert.AreEqual(Color.White, pattern.StripeAt(Tuple.Point(-1.1, 0, 0)));
+        }
+    }
+}
