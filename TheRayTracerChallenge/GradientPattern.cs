1633d616815f32bf7a2ef9505ffe5ec503aadb81
Carl Björknäs <carl.bjorknas@knowit.se>
2019-10-14 22:35:41 +0200

    Adds a gradient pattern.

---
+++ TheRayTracerChallenge/GradientPattern.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TheRayTracerChallenge
+{
+    internal class GradientPattern : Pattern
+    {
+        private Color _col1;
+        private Color _col2;
+
+        public GradientPattern(Color col1, Color col2)
+        {
+            this._col1 = col1;
+            this._col2 = col2;
+        }
+
+        public override Color ColorAt(Tuple point)
+        {
+            var distance = _col2 - _col1;
+            var fraction = point.x - Math.Floor(point.x);
+            return _col1 + distance * fraction;
+        }
+    }
+}
---
+++ TheRayTracerChallenge/Tests/GradientPatternTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheRayTracerChallenge.Tests
+{
+    [TestFixture]
+    class GradientPatternTests
+    {
+        [Test]
+        public void A_gradient_linearly_interpolates_between_colors()
+        {
+            var pattern = new GradientPattern(Color.White, Color.Black);
+            Assert.AreEqual(
+                new Color(1, 1, 1), 
+                pattern.ColorAt(Tuple.Point(0, 0, 0)));
+            Assert.AreEqual(
+                new Color(0.75, 0.75, 0.75), 
+                pattern.ColorAt(Tuple.Point(0.25, 0, 0)));
+            Assert.AreEqual(
+                new Color(0.5, 0.5, 0.5), 
+                pattern.ColorAt(Tuple.Point(0.5, 0, 0)));
+            Assert.AreEqual(
+                new Color(0.25, 0.25, 0.25), 
+                pattern.ColorAt(Tuple.Point(0.75, 0, 0)));
+        }
+    }
+}
