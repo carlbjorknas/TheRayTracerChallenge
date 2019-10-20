f23fe5a6ba8821b2f294619fbbe81daade61fd24
Carl Björknäs <carl.bjorknas@knowit.se>
2019-10-20 17:51:26 +0200

    Adds a checker pattern.

---
+++ TheRayTracerChallenge/Patterns/CheckerPattern.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheRayTracerChallenge.Patterns
+{
+    class CheckerPattern : Pattern
+    {
+        private readonly Color _col1;
+        private readonly Color _col2;
+
+        public CheckerPattern(Color col1, Color col2)
+        {
+            _col1 = col1;
+            _col2 = col2;
+        }
+
+        public override Color ColorAt(Tuple point)
+        {
+            var sum =
+                Math.Floor(point.x) +
+                Math.Floor(point.y) +
+                Math.Floor(point.z);
+
+            return sum % 2 == 0 ? Color.White : Color.Black;
+        }
+    }
+}
---
+++ TheRayTracerChallenge/Tests/Patterns/CheckerPatternTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TheRayTracerChallenge.Patterns;
+
+namespace TheRayTracerChallenge.Tests.Patterns
+{
+    [TestFixture]
+    class CheckerPatternTests
+    {
+        [Test]
+        public void Checkers_should_repeat_in_x()
+        {
+            var pattern = new CheckerPattern(Color.White, Color.Black);
+            Assert.AreEqual(Color.White, pattern.ColorAt(Tuple.Point(0, 0, 0)));
+            Assert.AreEqual(Color.White, pattern.ColorAt(Tuple.Point(0.99, 0, 0)));
+            Assert.AreEqual(Color.Black, pattern.ColorAt(Tuple.Point(1.01, 0, 0)));
+        }
+
+        [Test]
+        public void Checkers_should_repeat_in_y()
+        {
+            var pattern = new CheckerPattern(Color.White, Color.Black);
+            Assert.AreEqual(Color.White, pattern.ColorAt(Tuple.Point(0, 0, 0)));
+            Assert.AreEqual(Color.White, pattern.ColorAt(Tuple.Point(0, 0.99, 0)));
+            Assert.AreEqual(Color.Black, pattern.ColorAt(Tuple.Point(0, 1.01, 0)));
+        }
+
+        [Test]
+        public void Checkers_should_repeat_in_z()
+        {
+            var pattern = new CheckerPattern(Color.White, Color.Black);
+            Assert.AreEqual(Color.White, pattern.ColorAt(Tuple.Point(0, 0, 0)));
+            Assert.AreEqual(Color.White, pattern.ColorAt(Tuple.Point(0, 0, 0.99)));
+            Assert.AreEqual(Color.Black, pattern.ColorAt(Tuple.Point(0, 0, 1.01)));
+        }
+    }
+}
