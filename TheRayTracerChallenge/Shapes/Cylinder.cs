8265efeb605f764bca169f5398f8eca4dc23175c
Carl Björknäs <carl.bjorknas@gmail.com>
2020-12-19 21:02:05 +0100

    Tests that a ray misses the cylinder.

---
+++ TheRayTracerChallenge/Shapes/Cylinder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheRayTracerChallenge.Utils;
+
+namespace TheRayTracerChallenge.Shapes
+{
+    class Cylinder : Shape
+    {
+        public override IntersectionCollection LocalIntersect(Ray localRay)
+        {
+            var a = Math.Pow(localRay.Direction.x, 2) + Math.Pow(localRay.Direction.z, 2);
+            
+            if (a < C.Epsilon)
+                // Ray is parallell to the y axis
+                return new IntersectionCollection();
+
+            var b = 
+                2 * localRay.Origin.x * localRay.Direction.x +
+                2 * localRay.Origin.z * localRay.Direction.z;
+
+            var c = 
+                Math.Pow(localRay.Origin.x, 2) +
+                Math.Pow(localRay.Origin.z, 2) - 
+                1;
+
+            var disc = Math.Pow(b, 2) - 4 * a * c;
+
+            if (disc < 0)
+                // Ray does not intersect the cylinder
+                return new IntersectionCollection();
+
+            return new IntersectionCollection(new Intersection(1, this));
+        }
+
+        public override Tuple LocalNormalAt(Tuple localPoint)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TheRayTracerChallenge/Tests/Shapes/CylinderTests.cs
+++ TheRayTracerChallenge/Tests/Shapes/CylinderTests.cs
@@ -1,10 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NUnit.Framework;
+using FluentAssertions;
+using TheRayTracerChallenge.Shapes;
 
 namespace TheRayTracerChallenge.Tests.Shapes
 {
-    class CylinderTests
+    [TestFixture]
+    public class CylinderTests
     {
+        [Test]
+        public void A_ray_misses_a_cylinder()
+        {
+            A_ray_misses_a_cylinder(Tuple.Point(1, 0, 0), Tuple.Vector(0, 1, 0));
+            A_ray_misses_a_cylinder(Tuple.Point(0, 0, 0), Tuple.Vector(0, 1, 0));
+            A_ray_misses_a_cylinder(Tuple.Point(0, 0, -5), Tuple.Vector(1, 1, 1));
+        }
+
+        private void A_ray_misses_a_cylinder(Tuple origin, Tuple direction)
+        {
+            var cylinder = new Cylinder();
+            direction = direction.Normalize;
+            var ray = new Ray(origin, direction);
+            var xs = cylinder.Intersect(ray);
+            xs.Count.Should().Be(0);
+        }
     }
 }
