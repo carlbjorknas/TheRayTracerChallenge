665b6875f20146acba8a62f0b0bd7d71aaf7d892
Carl Björknäs <carl.bjorknas@gmail.com>
2020-12-26 14:50:14 +0100

    Adds a class for a group of shapes.

---
+++ TheRayTracerChallenge/Shapes/Group.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheRayTracerChallenge.Shapes
+{
+    class Group : Shape
+    {
+        public override IntersectionCollection LocalIntersect(Ray localRay)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Tuple LocalNormalAt(Tuple localPoint)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Shape> Shapes { get; } = new List<Shape>();
+    }
+}
---
+++ TheRayTracerChallenge/Tests/Shapes/GroupTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TheRayTracerChallenge.Shapes;
+
+namespace TheRayTracerChallenge.Tests.Shapes
+{
+    [TestFixture]
+    public class GroupTests
+    {
+        [Test]
+        public void Creating_a_new_group()
+        {
+            var group = new Group();
+            group.Transform.Should().Be(Transformation.Identity);
+            group.Shapes.Should().BeEmpty();
+        }
+    }
+}
