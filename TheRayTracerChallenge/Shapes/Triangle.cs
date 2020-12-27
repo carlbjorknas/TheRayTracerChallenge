f7b7559afa4245110f4ded3be746310688046706
Carl Björknäs <carl.bjorknas@gmail.com>
2020-12-27 18:29:54 +0100

    Started Triangle implementation.

---
+++ TheRayTracerChallenge/Shapes/Triangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheRayTracerChallenge.Shapes.Utils;
+using TheRayTracerChallenge.Utils;
+
+namespace TheRayTracerChallenge.Shapes
+{
+    class Triangle : Shape
+    {
+        public Triangle(Tuple p1, Tuple p2, Tuple p3)
+        {
+            P1 = p1;
+            P2 = p2;
+            P3 = p3;
+            EdgeVec1 = p2 - p1;
+            EdgeVec2 = p3 - p1;
+            Normal = EdgeVec2.Cross(EdgeVec1).Normalize;
+        }
+
+        public Tuple P1 { get; }
+        public Tuple P2 { get; }
+        public Tuple P3 { get; }
+        public Tuple EdgeVec1 { get; }
+        public Tuple EdgeVec2 { get; }
+        public Tuple Normal { get; }
+
+        public override Bounds Bounds => throw new NotImplementedException();
+
+        public override IntersectionCollection LocalIntersect(Ray localRay)
+        {
+            var dirCrossE2 = localRay.Direction.Cross(EdgeVec2);
+            var det = EdgeVec1.Dot(dirCrossE2);
+            if (Math.Abs(det) < C.Epsilon)
+                return new IntersectionCollection();
+
+            return new IntersectionCollection(new Intersection(1, this)); // Dummy to make sure the test works
+        }
+
+        public override Tuple LocalNormalAt(Tuple localPoint)
+        {
+            return Normal;
+        }
+    }
+}
---
+++ TheRayTracerChallenge/Tests/Shapes/TriangleTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TheRayTracerChallenge.Shapes;
+
+namespace TheRayTracerChallenge.Tests.Shapes
+{
+    [TestFixture]
+    public class TriangleTests
+    {
+        [Test]
+        public void Constructing_a_triangle()
+        {
+            var p1 = Tuple.Point(0, 1, 0);
+            var p2 = Tuple.Point(-1, 0, 0);
+            var p3 = Tuple.Point(1, 0, 0);
+            var triangle = new Triangle(p1, p2, p3);
+            triangle.P1.Should().Be(p1);
+            triangle.P2.Should().Be(p2);
+            triangle.P3.Should().Be(p3);
+            triangle.EdgeVec1.Should().Be(Tuple.Vector(-1, -1, 0));
+            triangle.EdgeVec2.Should().Be(Tuple.Vector(1, -1, 0));
+            triangle.Normal.Should().Be(Tuple.Vector(0, 0, -1));
+        }
+
+        [Test]
+        public void Finding_the_normal_on_a_triangle()
+        {
+            var triangle = new Triangle(Tuple.Point(0, 1, 0), Tuple.Point(-1, 0, 0), Tuple.Point(1, 0, 0));
+            var n1 = triangle.LocalNormalAt(Tuple.Point(0, 0.5, 0));
+            var n2 = triangle.LocalNormalAt(Tuple.Point(-0.5, 0.75, 0));
+            var n3 = triangle.LocalNormalAt(Tuple.Point(0.5, 0.25, 0));
+            n1.Should().Be(triangle.Normal);
+            n2.Should().Be(triangle.Normal);
+            n3.Should().Be(triangle.Normal);
+        }
+
+        [Test]
+        public void Intersecting_a_ray_parallel_to_the_triangle()
+        {
+            var triangle = new Triangle(Tuple.Point(0, 1, 0), Tuple.Point(-1, 0, 0), Tuple.Point(1, 0, 0));
+            var ray = new Ray(Tuple.Point(0, -1, -2), Tuple.Vector(0, 1, 0));
+            var xs = triangle.LocalIntersect(ray);
+            xs.Count.Should().Be(0);
+        }
+    }
+}
